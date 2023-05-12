73005a337f92b8f13d8ad93289ae11934de39641
hashZero1 <117970097+hashZero1@users.noreply.github.com>
2023-05-12 22:45:06 +0530

    Add files via upload

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -1,15 +1,14 @@
 import {Link} from 'react-router-dom'
 import './HomeStyles.scss'
 import './MediaQueries.scss'
-import 'animate.css'
-import Animate from 'animate.css'
+import "animate.css/animate.min.css";
 
 export default function Home(){
     return(
         <div className='container'>
            
             <h4>Let's Collaborate</h4>
-            <Animate animateIn='animate__fadeInUp' animatePreScroll={false}  animateOnce={true}>
+           
             <div className='contents'>
                 <div className='inner-content'>   
                     <h1>AI + RPA is what we do</h1>
@@ -17,15 +16,14 @@ export default function Home(){
                 <div className='inner-swap'>
                     <div className='inner-swap-content'>
                         <p>Future-Proof your business. Drive efficiency, profitability and deliver on customer experience</p>
-                        <Link to='/services/automation'>ai + rpa automation<span><img src='https://cdn1.iconfinder.com/data/icons/freeline/32/Arrow-Right-512.png'/></span></Link>
+                        <Link to='/services/automation'>ai + rpa automation<span><img src='https://cdn1.iconfinder.com/data/icons/freeline/32/Arrow-Right-512.png' alt='images'/></span></Link>
                     </div>
                     <div className='inner-img'>
                         <img src='https://www.xivtech.io./rp2.jpg' alt='robot'/>
                     </div>
                 </div>
             </div>
-            </Animate>
-            <Animate animateIn='animate__fadeInUp' animatePreScroll={false} animateOnce={true}>
+         
 
             <div className='contents'>
                 <div className='inner-content'>   
@@ -34,7 +32,7 @@ export default function Home(){
                 <div className='inner-swap'>
                   <div className='inner-swap-content'>
                      <p>Digital focused strategies to realize market-changing ideas</p>
-                     <Link to='/services/cloud-native'>Build Better App<span><img src='https://cdn1.iconfinder.com/data/icons/freeline/32/Arrow-Right-512.png'/></span></Link>
+                     <Link to='/services/cloud-native'>Build Better App<span><img src='https://cdn1.iconfinder.com/data/icons/freeline/32/Arrow-Right-512.png' alt='images'/></span></Link>
                   </div>
 
                   <div className='inner-img'>
@@ -42,8 +40,7 @@ export default function Home(){
                   </div>
                 </div>
             </div>
-            </Animate>
-            <Animate animateIn='animate__fadeInUp' animateOnce={true}>
+           
             <div className='contents'>
                 <div className='inner-content'>   
                     <h1>Innovate with Speed</h1>
@@ -51,7 +48,7 @@ export default function Home(){
                 <div className='inner-swap'>
                   <div className='inner-swap-content'>
                      <p>Create higher quality software, deliver on customer expectations and business goals</p>
-                     <Link to='/services/devops'>DevOps<span><img src='https://cdn1.iconfinder.com/data/icons/freeline/32/Arrow-Right-512.png'/></span></Link>
+                     <Link to='/services/devops'>DevOps<span><img src='https://cdn1.iconfinder.com/data/icons/freeline/32/Arrow-Right-512.png' alt='images'/></span></Link>
                   </div>
 
                   <div className='inner-img'>
@@ -59,8 +56,7 @@ export default function Home(){
                   </div>
                 </div>
             </div>
-            </Animate>
-            <Animate animateIn='animate__fadeInUp' animateOnce={true}>
+          
             <div className='contents'>
                 <div className='inner-content'>   
                     <h1>Embrace Cloud</h1>
@@ -68,7 +64,7 @@ export default function Home(){
                 <div className='inner-swap'>
                   <div className='inner-swap-content'>
                      <p>With Cloud-First accelerate innovation and optimize performance</p>
-                     <Link to='/services/cloud-transition'>cloud services<span><img src='https://cdn1.iconfinder.com/data/icons/freeline/32/Arrow-Right-512.png'/></span></Link>
+                     <Link to='/services/cloud-transition'>cloud services<span><img src='https://cdn1.iconfinder.com/data/icons/freeline/32/Arrow-Right-512.png' alt='images'/></span></Link>
                   </div>
 
                   <div className='inner-img'>
@@ -76,7 +72,6 @@ export default function Home(){
                   </div>
                 </div>
             </div>
-            </Animate>
             <div className='wrapper extra-links'>
             <button><span>Get in touch</span></button>
             </div>
